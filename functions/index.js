05ad00987ed195dbe63b983eba6cc2803f9114a4
Amanda Caruso Cavallaro <chibi@Amandas-MacBook-Pro.local>
2019-01-20 02:56:30 +0000

    Add commented code for adding firebase database querying

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,13 @@ const functions = require('firebase-functions');
 const { WebhookClient } = require('dialogflow-fulfillment');
 const { Card, Suggestion } = require('dialogflow-fulfillment');
 
+// Firebase real time database info
+// var admin = require(`firebase-admin`);
+// admin.initializeApp({
+//   databaseURL: ``
+// });
+// var db = admin.database();
+
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
@@ -21,15 +28,12 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     agent.add(`I'm sorry, can you try again?`);
   }
 
+  // Add Firebase call here
   function mentorSearch(agent) {
-    console.log("mentor");
-    console.log(request.body.queryResult.languageCode);
     agent.add(`This is their contact: mentor.contact. Would you like to call her?`);
   }
 
   function transferCall(agent) {
-    console.log("transferCall");
-    console.log(request.body.queryResult.sentimentAnalysisResult);
     agent.add(`Transfering you now!`);
   }
 
@@ -39,8 +43,43 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 
   // transfers the call in case someone says something negative
   if (request.body.queryResult.sentimentAnalysisResult.queryTextSentiment.score < 0) {
+    console.log(request.body.queryResult.sentimentAnalysisResult.queryTextSentiment.score);
     let callEvent = agent.setFollowupEvent('telephone-event');
+  } else {
+    console.log(request.body.queryResult.sentimentAnalysisResult.queryTextSentiment.score);
   }
+
+  // Fetching information from firebase real time database
+  // function searchName(agent) {
+  //   var results = db.ref("myDatabase");
+  //   results
+  //     .orderByChild("Name")
+  //     .startAt(agent.parameters.person_name)
+  //     .endAt(`${agent.parameters.person_name}~`)
+  //     .on("value", function(snapshot) {
+  //       snapshot.forEach(function(data) {
+  //         console.log(`The mentor's e-mail is ${data.val().Email}`);
+  //         agent.add(`The mentor's e-mail is ${data.val().Email}`);
+  //       });
+  //     });
+  // }
+
+  // function searchSkill(agent) {
+  //   var scoresRef = db.ref("myDatabase");
+  //   scoresRef
+  //     .orderByChild("Skill")
+  //     .equalTo(agent.parameters.person_skill)
+  //     .on("value", function(snapshot) {
+  //       snapshot.forEach(function(data) {
+  //         agent.add(
+  //           "The mentor is an expert at " +
+  //             data.val().Skill +
+  //             " is " +
+  //             data.val().Name
+  //         );
+  //       });
+  //     });
+  // }
 
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
